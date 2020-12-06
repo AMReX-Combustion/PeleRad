ed53a27a33f0d8593472c1ba3da0571cae9fe78a
w0g <gew1@ornl.gov>
2020-12-06 00:23:25 -0500

    use GpuArray on managed memory for all tests; update ci to Ubuntu 20.10;

--- tests/tstBoostReadDatabase.cpp
+++ tests/tstBoostReadDatabase.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <vector>
 
+#include <AMReX_MultiFab.H>
+
 namespace but = boost::unit_test;
 namespace bfs = boost::filesystem;
 namespace bio = boost::iostreams;
@@ -28,18 +30,21 @@ BOOST_AUTO_TEST_CASE(boost_read_database, *but::tolerance(0.00001))
 
     bio::stream<bio::mapped_file_source> data(kplco2);
 
-    std::vector<float> T;
-    std::vector<float> kpco2;
+    // std::vector<float> T;
+    // std::vector<float> kpco2;
+
+    amrex::GpuArray<amrex::Real, 125ul> T;
+    amrex::GpuArray<amrex::Real, 125ul> kpco2;
 
     size_t i = 0;
 
     for (float T_temp, kp_temp; data >> T_temp >> kp_temp;)
     {
         // std::cout << "Reading from file:" << T << " " << kp << '\n';
-        T.push_back(T_temp);
-
-        kpco2.push_back(kp_temp);
-
+        // T.push_back(T_temp);
+        // kpco2.push_back(kp_temp);
+        T[i]     = T_temp;
+        kpco2[i] = kp_temp;
         i++;
     }
 
